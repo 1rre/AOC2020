330dbce417328359e8df1ac0ec0d8e4b0c5df3d0
tjm1518 <tjm1518@ic.ac.uk>
2020-12-18 10:28:17 +0000

    Formatting

--- day_18/day_18.erl
+++ day_18/day_18.erl
@@ -2,7 +2,9 @@
 -export ([main/1]).
 
 main(_Args) ->
-  io:fwrite("~B~n", [lists:sum([calc(element(2, parser:parse(element(2, lexer:string(X))))) || X <- string:split(binary_to_list(string:trim(element(2, file:read_file("input")))), "\n", all)])]).
+  Result = lists:sum([calc(element(2, parser:parse(element(2, lexer:string(X))))) ||
+                      X <- string:split(binary_to_list(string:trim(element(2, file:read_file("input")))), "\n", all)]),
+  io:fwrite("~B~n", [Result]).
 
 
 calc({num, _, X}) -> X;
